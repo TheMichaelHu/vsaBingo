fc948a4398010ceaeac46fc6e21e9157abb2b52c
Michael <michaelanhu@gmail.com>
2016-02-29 21:31:44 -0500

    added lobby page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'lobby#index'
+  resources :room, except: [:index, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
