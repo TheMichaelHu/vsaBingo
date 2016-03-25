9bd472e37980736cd524949e9cf45ac17090411f
Michael <michaelanhu@gmail.com>
2016-03-24 23:54:29 -0400

    heartbeat logging

--- config/events.rb
+++ config/events.rb
@@ -1,5 +1,4 @@
 WebsocketRails::EventMap.describe do
-  subscribe :bingo_message, to: MessagesController, with_method: :message_receive
   subscribe :room_message, to: MessagesController, with_method: :handle_room_message
   subscribe :bingo_message, to: MessagesController, with_method: :handle_bingo_message
 end
--- config/initializers/websocket_rails.rb
+++ config/initializers/websocket_rails.rb
@@ -1,4 +1,5 @@
 WebsocketRails.setup do |config|
+  config.log_internal_events = true
   config.standalone = true
   if Rails.env.production?
     config.redis_options = {:host => 'tarpon.redistogo.com', :port => '10810', :user => 'redistogo', :password => '222473d9cfb996ef1feb29257334f40b'}
